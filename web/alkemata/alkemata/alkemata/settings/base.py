c0178d13b713495c414eb9146c8831f8ec281de3
alkemata <info@alkemata.com>
2018-08-14 21:08:28 +0200

    beginning topics

--- web/alkemata/alkemata/alkemata/settings/base.py
+++ web/alkemata/alkemata/alkemata/settings/base.py
@@ -27,6 +27,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
+    'user',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'wagtail.search',
     'wagtail.admin',
     'wagtail.core',
-
     'modelcluster',
     'taggit',
 
@@ -154,3 +154,8 @@ WAGTAIL_SITE_NAME = "alkemata"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://alkemata.com'
+
+AUTH_USER_MODEL = 'user.User'
+WAGTAIL_USER_EDIT_FORM = 'user.forms.CustomUserEditForm'
+WAGTAIL_USER_CREATION_FORM = 'user.forms.CustomUserCreationForm'
+WAGTAIL_USER_CUSTOM_FIELDS = ['role', 'profile_image']
--- web/alkemata/alkemata/home/models.py
+++ web/alkemata/alkemata/home/models.py
@@ -7,7 +7,14 @@ from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.admin.edit_handlers import PageChooserPanel
 from wagtail.search import index
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import  StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
+
 
 
 class HomePage(Page):
@@ -19,7 +26,7 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-        news_page = models.ForeignKey(
+    news_page = models.ForeignKey(
         'wagtailcore.Page',
         null=True,
         blank=True,
@@ -33,6 +40,7 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('headline', classname="full"),
         FieldPanel('messages', classname="full"),
+        PageChooserPanel('news_page', 'home.NewsIndexPage'),
     ]
     promote_panels = [
         MultiFieldPanel(Page.promote_panels, "Common page configuration"),
@@ -45,13 +53,16 @@ class NewsIndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
+
+    def latest(self):
+        return NewsPage.objects.child_of(self).live()
     
     def get_context(self, request):
         context = super(NewsIndexPage, self).get_context(request)
 
         # Get the full unpaginated listing of resource pages as a queryset -
         # replace this with your own query as appropriate
-        all_resources = NewsPage.objects.live()
+        all_resources = NewsPage.objects.live().descendant_of(self)
 
         paginator = Paginator(all_resources, 5) # Show 5 resources per page
 
@@ -67,7 +78,6 @@ class NewsIndexPage(Page):
 
         # make the variable 'resources' available on the template
         context['news'] = resources
-        context['latestnews']=all_ressources(0:5)
         return context
 
 
@@ -87,5 +97,57 @@ class NewsPage(Page):
         FieldPanel('body', classname="full"),
     ]
 
-    parent_page_types = ['alkemata.NewsIndex']
+    parent_page_types = ['NewsIndexPage']
     subpage_types = []
+
+class TopicIndexPage(Page):
+        intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+    def latest(self):
+        return TopicPage.objects.child_of(self).live()
+    
+    def get_context(self, request):
+        context = super(TopicIndexPage, self).get_context(request)
+
+        # Get the full unpaginated listing of resource pages as a queryset -
+        # replace this with your own query as appropriate
+        all_resources = TopicPage.objects.live().descendant_of(self)
+
+        paginator = Paginator(all_resources, 5) # Show 5 resources per page
+
+        page = request.GET.get('page')
+        try:
+            resources = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            resources = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            resources = paginator.page(paginator.num_pages)
+
+        # make the variable 'resources' available on the template
+        context['topics'] = resources
+        return context
+
+class TopicPage(Page):
+    author = models.CharField(max_length=255)
+    date = models.DateField("Post date")
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('Articles_list', blocks.ListBlock(blocks.StructBlock([
+    ('article', blocks.PageChooserBlock(required=False)),
+])))
+
+    ])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('author'),
+        FieldPanel('date'),
+        StreamFieldPanel('body'),
+    ]
